e4c478657db759e4306c4029a0e98c4f3e7e9844
Elena Timofeeva <posazho@gmail.com>
2022-07-01 20:55:03 +0300

    показыавем точки в четверти

---
+++ Task18/Program.cs
@@ -0,0 +1,29 @@
+﻿ // Напишите программу, которая по заданному номеру четверти, 
+ //показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти: ");
+int QuarterNumber = Convert.ToInt32(Console.ReadLine());
+if (QuarterNumber > 0 && QuarterNumber < 5)
+{
+    if (QuarterNumber == 1)
+    {
+        Console.WriteLine($"В {QuarterNumber} четверти  X > 0 и Y > 0 .");
+    }
+    else if (QuarterNumber == 2)
+    {
+        Console.WriteLine($"В {QuarterNumber} четверти  X < 0 и Y > 0 .");
+    }
+    else if (QuarterNumber == 3)
+    {
+        Console.WriteLine($"В {QuarterNumber} четверти  X < 0 и Y < 0 .");
+    }
+     else if (QuarterNumber == 4)
+    {
+        Console.WriteLine($"В {QuarterNumber} четверти  X > 0 и Y < 0 .");
+    }
+
+}
+else
+{
+    Console.WriteLine("Число должно быть в диапазоне от 1 до 4");
+}
