e19700a69f0dff959c802c536d1be36dd349496c
Elena Timofeeva <posazho@gmail.com>
2022-07-03 16:28:19 +0300

    расстояние между двумя точками в трехмерном пространстве.корректировка

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -16,6 +16,5 @@ Console.WriteLine("Введите координату Y второй точки
 int userY2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату Z второй точки: ");
 int userZ2 = Convert.ToInt32(Console.ReadLine());
-
 double distance = Math.Sqrt(Math.Pow(userX1 - userX2,2) + Math.Pow(userY1 - userY2,2) + Math.Pow(userZ1 - userZ2,2));
 Console.WriteLine($"Расстояние между двумя точками [{userX1}, {userY1}, {userZ1}]:[{userX2},{userY2}, {userZ2}] равно {Math.Round(distance,2)} ");
