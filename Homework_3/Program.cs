8c77775773b0fa6fa0b4f38e929c244e0437981a
Elena Timofeeva <posazho@gmail.com>
2022-07-03 16:37:59 +0300

    выдача таблицы кубов чисел от 1 до N

---
+++ Homework_3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход число N и выдает 
+таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < N + 1; i++)
+{
+    int cube = i * i *i;
+    if (i == N)
+    {
+        Console.Write($" {cube}");
+    }
+    else
+    {
+        Console.Write($" {cube},");
+    }
+}
